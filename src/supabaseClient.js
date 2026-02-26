49e1781c0157c73e640abfffdc89ef97c3d91ed4
Teu Nome de Utilizador do GitHub <o-teu-email@exemplo.com>
2026-02-26 00:08:54 +0000

    fix: use environment variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Substitui as tuas linhas 3 e 4 por estas:
+import { createClient } from '@supabase/supabase-js'
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
